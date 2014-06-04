aed6d5f3ee83a04eae28c65874497aa3dbfaf7d0
Hackbright Student <info@hackbrightacademy.com>
2014-06-04 16:43:20 -0700

    Initial commit for exercise 3

---
+++ calculator.py
@@ -0,0 +1,16 @@
+"""
+calculator.py
+
+Using our arithmetic.py file from Exercise02, create the
+calculator program yourself in this file.
+"""
+
+
+def main():
+    while True:
+        input = raw_input()
+
+        print input + "hello"
+
+if __name__ == '__main__':
+    main()
