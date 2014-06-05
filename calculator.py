436c545bd3c4693c620b5e4aed1807f1da56dddd
Hackbright Student <info@hackbrightacademy.com>
2014-06-04 17:07:42 -0700

    added dummy functions but it does not yet point to arithmetic file

--- calculator.py
+++ calculator.py
@@ -14,5 +14,25 @@ def main():
         if split_string[0] == 'q':
             break
 
+        else:
+            if split_string[0] == '+':
+                return "Someday I will know how to add!"
+            elif split_string[0] == '-':
+                return "Someday I will know how to subtract!"
+            elif split_string[0] == '*':
+                return "Someday I will know how to multiply!"
+            elif split_string[0] == '/':
+                return "Someday I will know how to divide!"
+            elif split_string[0] == 's':
+                return "Someday I will know how to square!"
+            elif split_string[0] == 'c':
+                return "Someday I will know how to cube!"
+            elif split_string[0] == 'p':
+                return "Someday I will know how to do powers!"
+            elif split_string[0] == 'm':
+                return "Someday I will know how to do modulus!"
+            else:
+                print "I don't understand." 
+
 if __name__ == '__main__':
     main()
